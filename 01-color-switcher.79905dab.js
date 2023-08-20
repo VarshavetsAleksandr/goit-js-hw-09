e67cd9d2836ad2e8cd0ae485712691005dcf9eda
VarshavetsAleksandr <128036812+VarshavetsAleksandr@users.noreply.github.com>
2023-08-20 22:40:20 +0000

    Deploying to gh-pages from @ VarshavetsAleksandr/goit-js-hw-09@2092efb2792871ed8f5f23680e96a190b2c2a63b 🚀

--- 01-color-switcher.79905dab.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.btnStart.addEventListener("click",(function(){return t.btnStart.disabled=!0,t.btnStop.disabled=!1,id=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(id)})),t.btnStop.disabled=!0;
-//# sourceMappingURL=01-color-switcher.79905dab.js.map
