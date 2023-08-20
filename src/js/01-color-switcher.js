4fa560a67f29278968ba65999f17de7c2cc64217
VarshavetsAleksandr <varshavetsaleksandr@gmail.com>
2023-08-20 23:19:12 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const elements = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+elements.btnStart.addEventListener('click', handlerStart);
+elements.btnStop.addEventListener('click', handlerStop);
+
+function handlerStart() {
+  elements.btnStart.disabled = true;
+  return (id = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000));
+}
+
+function handlerStop() {
+  elements.btnStart.disabled = false;
+  clearInterval(id);
+}
